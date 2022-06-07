80ebab62a7adf7c40d976684c692418e0cd77099
Varshen <varshen3@gmail.com>
2022-06-07 10:38:56 -0700

    xx

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** main - check the code 
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar9i0;
+	_putchar('\n');
+}
